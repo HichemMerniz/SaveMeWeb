5dfe5e43f52ab14ef86b10178b95c3d2e30b1f56
HichemMerniz <38223636+HichemMerniz@users.noreply.github.com>
2018-12-07 21:06:37 +0100

    up

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,19 @@
  Route::get('/', function () {
     $doctors=   DB::table('doctors')->orderBy('status', 'asc')->get();
      return view('accueil')->with(['doctors'=>$doctors]);
+     
  });
  Route::get('/mapview', function () {
-     $users =  DB::table('users')->where('status',1)->first();
+    $users =  DB::table('users')->where('status',1)->first();
     return view('mapview')->with(['users'=>$users]);
 });
-
 Route::get('/doctors', function () {
-    return view('doctor','DoctorController@index');
+    $doctors=   DB::table('doctors')->orderBy('status', 'asc')->get();
+     return view('doctor')->with(['doctors'=>$doctors]);
 });
+
 Route::get('/userposi','emergencyLocation@saveLocation');
 Route::get('/help','emergencyLocation@sendHelp');
 Route::get('/newuser','newuser@saveUser');
 Route::post('/ambuPosi','emergencyLocation@ambuPosi');
 Route::post('/ambuPosiReq','emergencyLocation@ambuPosiReq');
-
-//Route::get('/','DoctorController@index');
